552322e373df07b001c39145b611818f3e071e58
Lemsito22 <lemquilca22@gmail.com>
2025-11-26 10:00:03 +0100

    ...

--- main/src/pin4numeros.java
+++ main/src/pin4numeros.java
@@ -5,20 +5,26 @@ public class pin4numeros {
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingresa un pin de cuatro numeros");
 
-    while (!sc.hasNextInt()){
+      while (!sc.hasNextInt()){
         System.out.println("Pin no valido");
         sc.next();
-    }
-        boolean continua = true;
-        while (continua) {
-            int num2 = sc.nextInt();
-            if (num2 > 2025) {
-                System.out.println("No creo que seas del futuro, ingresa un numero valido.");
-            }  else {
-                int edad = 2025 - num2;
-                System.out.println("Tienes " + edad+" años.");
-                continua=false;
-            }
-        }
+      }
+     boolean cumple=true;
+       while (cumple) {
+           int num2 = sc.nextInt();
+
+           if (String.valueOf(num2).length() == 4) {
+               System.out.println("Numero valido: " + num2);
+               System.out.println("Longitud: " + String.valueOf(num2).length());
+               cumple=false;
+           } else if (String.valueOf(num2).length() < 4) {
+               System.out.println("Longitud: " + String.valueOf(num2).length());
+               System.out.println("La longitud debe ser de 4 caracteres, escribe un Pin que cumpla con esa condición");
+           } else if (String.valueOf(num2).length() > 4) {
+               System.out.println("Longitud: " + String.valueOf(num2).length());
+               System.out.println("La longitud debe ser de 4 caracteres, escribe un Pin que cumpla con esa condición");
+           }
+
+       }
 }
 }
