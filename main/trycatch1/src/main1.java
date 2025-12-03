2dba283bff138a7e27cbfb8e1e7a73b1fe550fb3
Lemsito22 <lemquilca22@gmail.com>
2025-12-03 09:26:22 +0100

    ...

--- main/trycatch1/src/main1.java
+++ main/trycatch1/src/main1.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class main1 {
@@ -5,15 +6,40 @@ public class main1 {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Bienvenido a la calculadora");
-        System.out.println("Ingresa el primer numero: ");
-        double n1=sc.nextDouble();
-        System.out.println("Ingresa la operacion que quieres realizar (+),(-),(*),()");
-        try {
-            System.out.println();
-        }
-        catch (Exception e) {
-            System.out.println("No se puede hacer una división entre 0");
-            System.out.println("Error: "+ e);
+        boolean salir=false;
+        while (!salir) {
+
+            try {
+                System.out.println("Ingresa el primer numero: ");
+                int n1 = sc.nextInt();
+                System.out.println("Ingresa la operacion que quieres realizar (+),(-),(*),(/)");
+                String operador = sc.next();
+                System.out.println("Ingresa el segundo numero: ");
+                int n2 = sc.nextInt();
+                int resultado;
+                if (operador.equalsIgnoreCase("+")) {
+                    resultado = n1 + n2;
+                    System.out.println("El resultado de " + n1 + "+" + n2 + " es: " + resultado);
+                } else if (operador.equalsIgnoreCase("-")) {
+                    resultado = n1 - n2;
+                    System.out.println("El resultado de " + n1 + "-" + n2 + " es: " + resultado);
+                } else if (operador.equalsIgnoreCase("*")) {
+                    resultado = n1 * n2;
+                    System.out.println("El resultado de " + n1 + "*" + n2 + " es: " + resultado);
+                } else if (operador.equalsIgnoreCase("/")) {
+                    resultado = n1 / n2;
+                    System.out.println("El resultado de " + n1 + "/" + n2 + " es: " + resultado);
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Error, los datos no son numericos, vuelve a intentarlo");
+                System.out.println("Error: " + e);
+                sc.next();
+            } catch (ArithmeticException e){
+                System.out.println("No se puede dividir un numero entre 0");
+                System.out.println("Error: "+e);
+            } catch (Exception a) {
+                System.out.println(a);
+            }
         }
     }
 }
